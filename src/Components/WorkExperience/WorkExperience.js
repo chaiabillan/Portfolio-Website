081edd88176f1149445e3a0b6bd18103287d4414
Chaia Billan <chaia_billan@yahoo.com>
2024-04-29 12:52:33 -0700

    finish resume

--- src/Components/WorkExperience/WorkExperience.js
+++ src/Components/WorkExperience/WorkExperience.js
@@ -48,17 +48,6 @@ function WorkExperience({ experiences, showAllExperiences, setShowAllExperiences
             )}
         </section>
     );
-    // return (
-    //     <section className='work-experience selected'  ref={experienceRef}>
-    //         {experiences.map((experience, index) => (
-    //             <button key={index} className={`experience ${experience.className}`} onClick={experience.onClick}>
-    //                 <p className='experience__company'>{experience.company}</p>
-    //                 <p className='experience__role'>{experience.role}</p>
-    //                 <p className='experience__date'>{experience.date}</p>
-    //             </button>
-    //         ))}
-    //     </section>
-    // )
 }
 
 export default WorkExperience
